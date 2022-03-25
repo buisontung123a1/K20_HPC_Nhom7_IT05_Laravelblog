272cb58985fcac1e7e7df16d757b97ff439e5382
tranthinh872 <thinhtit27@gmail.com>
2022-03-26 00:37:52 +0700

    tao them blade

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/tables-basic', function () {
+    return view('tables-basic');
+});
+Route::get('/index1', function () {
+    return view('index1');
+});
+Route::get('/pages-misc-error', function () {
+    return view('pages-misc-error');
+}) ->name('admin.pages-misc-error');
+
+Route::get('/auth-forgot-password-basic', function () {
+    return view('auth-forgot-password-basic');
+})->name('admin.auth-forgot-password-basic');
