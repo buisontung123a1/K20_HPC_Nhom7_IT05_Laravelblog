3cf9e906ecbc057fedf4f61fba0cc7de22c1924a
dokimcuong <dokimcuong.hp00@gmail.com>
2022-03-25 21:23:12 +0700

    code

--- test/routes/web.php
+++ test/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\adminlogin;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,7 +22,34 @@ Route::get('/tables-basic', function () {
 });
 Route::get('/index1', function () {
     return view('index1');
-});
+}) -> name('index');
 Route::get('/pages-misc-error', function () {
     return view('pages-misc-error');
-}) ->name('admin.pages-misc-error');
+}) -> name('admin.pages-misc-error');
+Route::get('/pages-misc-under-maintenance', function () {
+    return view('pages-misc-under-maintenance');
+});
+Route::get('/layouts-without-menu', function(){
+    return view('layouts-without-menu');
+});
+Route::get('/auth-login-basic', function(){
+    return view('auth-login-basic');
+});
+Route::get('/auth-register-basic', function(){
+    return view('auth-register-basic');
+});
+Route::get('/auth-forgot-password-basic', function(){
+    return view('auth-forgot-password-basic');
+});
+Route::get('/pages-account-settings-account', function(){
+    return view('pages-account-settings-account');
+});
+Route::get('/pages-account-settings-connections', function(){
+    return view('pages-account-settings-connections');
+});
+Route::get('/pages-account-settings-notifications', function(){
+    return view('pages-account-settings-notifications');
+});
+Route::get('/cards-basic', function(){
+    return view('cards-basic');
+});
